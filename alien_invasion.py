6c6822b4b55f51eb1fae392890408b538f193606
知不可乎 <2297937926@qq.com>
2022-02-08 20:30:36 +0800

    Initial commit

---
+++ alien_invasion.py
@@ -0,0 +1,37 @@
+import pygame
+from settings import Settings
+from game_stats import GameStats
+from scoreboard import Scoreboard
+from button import Button
+from ship import Ship
+import game_funcions as gf
+from pygame.sprite import Group
+
+
+def run_game():
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption('Alien Invasion')
+
+    play_button = Button(ai_settings, screen, "Play")
+    stats = GameStats(ai_settings)
+    sb = Scoreboard(ai_settings, screen, stats)
+    ship = Ship(ai_settings, screen)
+    bullets = Group()
+    aliens = Group()
+
+    gf.creat_fleet(ai_settings, screen, ship, aliens)
+
+    while True:
+        gf.check_events(ai_settings, screen, stats, sb, play_button, aliens, ship, bullets)
+
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(stats, sb, ai_settings, screen, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, stats, sb, screen, ship, aliens, bullets)
+
+        gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button)
+
+
+run_game()
